19b9debbfa2e46f412af2f6cb0e97050d28801cb
MacBook Air <macbookair@MacBook-Air-MacBook-4.local>
2026-02-24 23:09:16 +0500

    add

--- src/pages/OnlineCourse/OnlineCourse.jsx
+++ src/pages/OnlineCourse/OnlineCourse.jsx
@@ -1,4 +1,3 @@
-import { CheckCircle2 } from "lucide-react";
 import React from "react";
 import CourseList from "./components/CourseList";
 import { useTranslation } from "react-i18next";
@@ -28,12 +27,12 @@ const OnlineCourse = () => {
         <section className="py-8 md:px-6 mt-16">
           <div className="max-w-4xl mx-auto text-center">
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-gray-900 mb-4 text-balance">
-                Online Courses
+              {t("Online courses")}
             </h1>
             <p className="text-base md:text-lg text-gray-600 max-w-2xl mx-auto text-balance">
-              Al Muamalat Education's international study programs offer an
-              in-depth learning experience at leading Islamic financial
-              institutions around the world.
+              {t(
+                "Al Muamalat Education's international study programs offer an in-depth learning experience at leading Islamic financial institutions around the world.",
+              )}
             </p>
           </div>
         </section>
